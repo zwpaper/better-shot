cad90ca8834abf42334800ef8f99b27fff0caec8
KartikLabhshetwar <kartik.labhshetwar@gmail.com>
2026-01-14 15:33:37 +0530

    chore: update download links to v0.1.8

--- bettershot-landing/lib/downloads.ts
+++ bettershot-landing/lib/downloads.ts
@@ -1,7 +1,7 @@
 export const downloadLinks = {
   appleSilicon:
-    "https://github.com/KartikLabhshetwar/better-shot/releases/download/v0.1.7/bettershot_0.1.7_aarch64.dmg",
+    "https://github.com/KartikLabhshetwar/better-shot/releases/download/v0.1.8/bettershot_0.1.8_aarch64.dmg",
   intel:
-    "https://github.com/KartikLabhshetwar/better-shot/releases/download/v0.1.7/bettershot_0.1.7_x64.dmg",
+    "https://github.com/KartikLabhshetwar/better-shot/releases/download/v0.1.8/bettershot_0.1.8_x64.dmg",
 } as const
 
